bcca13d9d67179f03fdb0637eb891fa35582289b
gabrielle giannoni <gabrielleggiannoni@gmail.com>
2023-05-04 17:24:18 -0300

    criação modal cadastro usuario

--- src/components/CardUser.js
+++ src/components/CardUser.js
@@ -3,9 +3,9 @@ import Box from '@mui/material/Box'
 const CardUser = ({user}) => {
   return (
     <Box sx={{background: '#D3D3D3', borderRadius: '10px', display: 'flex', margin: '20px 0', padding: '15px'}}>
-        <img src={user.avatar} alt={user.name} style={{width: '80px',height: '80px', borderRadius: '50%'}}/>
+        <img src={user.foto} alt={user.nome} style={{width: '80px',height: '80px', borderRadius: '50%'}}/>
         <Box>
-            <h3>{user.name}</h3>
+            <h3>{user.nome}</h3>
             <p>{user.email}</p>
         </Box>
     </Box>
